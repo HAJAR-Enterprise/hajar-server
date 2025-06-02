5f203025b233901bbebfbb91b90c30d8fa9221f2
LinggarR01 <riza.linggar@gmail.com>
2025-06-02 23:20:41 +0700

    Reapply "add: jwt_token, save user data into firestore, and get user videos"
    This reverts commit c1059a04d81a65daac8409f5b551584ee1a7bae3.

--- src/handlers/authHandler.js
+++
@@ -1,9 +0,0 @@
-const { verifyIdToken } = require('../services/firebaseService');
-
-const verifyFirebaseUser = async (request, h) => {
-  const token = request.headers.authorization?.replace('Bearer ', '');
-  const user = await verifyIdToken(token); 
-  return h.response({ message: 'Authorized', user });
-};
-
-module.exports = { verifyFirebaseUser };
